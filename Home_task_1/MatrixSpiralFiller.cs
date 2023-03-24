f1012c8ef157735c4d7444f9228f2d7c878e15b2
FVladislove <f.vlad.is.love@gmail.com>
2023-03-24 21:35:04 +0200

    task-1 done

---
+++ Home_task_1/MatrixSpiralFiller.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_task_1
+{
+    internal static class MatrixSpiralFiller
+    {
+        public static void FillMatrixInSpirall(int[,] matrix, bool counterclockwise = true)
+        {
+            if (counterclockwise)
+            {
+                FillMatrixInSpiralCounterclockwise(matrix);
+            }
+            FillMatrixInSpiralClockwise(matrix);
+        }
+
+        private static void FillMatrixInSpiralCounterclockwise(int[,] matrix)
+        {
+            int rows_count = matrix.GetLength(0);
+            int cols_count = matrix.GetLength(1);
+            int operationsCount = rows_count * cols_count;
+            // 0 => down, 1 => right, 2 => top, 3 => left
+            int direction = 0;
+            int row = 0, col = 0;
+            int boundary = rows_count - 1;
+
+            for (int i = 1; i <= operationsCount; i++)
+            {
+                matrix[row, col] = i;
+
+                switch (direction)
+                {
+                    case 0:
+                        row++;
+                        break;
+                    case 1:
+                        col++;
+                        break;
+                    case 2:
+                        row--;
+                        break;
+                    case 3:
+                        col--;
+                        break;
+                }
+
+                if (i == boundary)
+                {
+                    direction = (direction + 1) % 4;
+                    boundary += (direction % 2 == 0) ? --rows_count : --cols_count;
+                }
+            }
+        }
+
+        private static void FillMatrixInSpiralClockwise(int[,] matrix)
+        {
+            int rows_count = matrix.GetLength(0);
+            int cols_count = matrix.GetLength(1);
+            int operationsCount = rows_count * cols_count;
+            // 0 => right, 1 => bottom, 2 => left, 3 => top
+            int direction = 0;
+            int row = 0, col = 0;
+            int boundary = cols_count - 1;
+
+            for (int i = 1; i <= operationsCount; i++)
+            {
+                matrix[row, col] = i;
+
+                switch (direction)
+                {
+                    case 0:
+                        col++;
+                        break;
+                    case 1:
+                        row++;
+                        break;
+                    case 2:
+                        col--;
+                        break;
+                    case 3:
+                        row--;
+                        break;
+                }
+
+                if (i == boundary)
+                {
+                    direction = (direction + 1) % 4;
+                    boundary += (direction % 2 == 0) ? --cols_count : --rows_count;
+                }
+            }
+        }
+    }
+}
---
+++ Home_task_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using Home_task_1;
+
+int n = 3;
+int m = 4;
+
+int[,] matrix = new int[n, m];
+
+MatrixSpiralFiller.FillMatrixInSpirall(matrix, false);
+
+for(int i = 0; i < matrix.GetLength(0); i++)
+{
+    for(int j = 0; j < matrix.GetLength(1); j++)
+    {
+        Console.Write("{0, 4}", matrix[i, j]);
+    }
+    Console.WriteLine();
+}
