0a603ed0e01b1b20bf681e9caa629b6fa94162ac
FVladislove <f.vlad.is.love@gmail.com>
2023-03-27 03:00:01 +0300

    added user input

--- Home_task_1/TasksRepresenter.cs
+++ Home_task_1/TasksRepresenter.cs
@@ -10,13 +10,12 @@ namespace Home_task_1
         public static void Task1_SpiralMatrix()
         {
             Console.WriteLine("==============Task1==============");
-            // TODO add user input
-            int n = 3;
-            int m = 4;
-
+            int n = ReadIntNumber("rows", Validator.ValidateSequenceSizeInput);
+            int m = ReadIntNumber("cols", Validator.ValidateSequenceSizeInput);
             int[,] matrix = new int[n, m];
 
-            MatrixSpiralFiller.FillMatrixInSpirall(matrix, false);
+            // TODO add user selection, maybe
+            MatrixSpiralFiller.FillMatrixInSpirall(matrix);
 
             for (int i = 0; i < matrix.GetLength(0); i++)
             {
@@ -31,9 +30,15 @@ namespace Home_task_1
         public static void Task2_LongestColorFind()
         {
             Console.WriteLine("==============Task2==============");
-            // TODO add user input
+
+            int n = ReadIntNumber("rows", Validator.ValidateSequenceSizeInput);
+            int m = ReadIntNumber("cols", Validator.ValidateSequenceSizeInput);
+
+            int[,] matrix = new int[n, m];
             var rnd = new Random();
-            int[,] matrix = new int[7, 8];
+
+            Console.WriteLine("Created matrix with random numbers");
+
             for (int i = 0; i < matrix.GetLength(0); i++)
             {
                 for (int j = 0; j < matrix.GetLength(1); j++)
@@ -50,6 +55,52 @@ namespace Home_task_1
 
             Console.WriteLine("Color info:");
             Console.WriteLine(colorFinder);
+        }
+
+        public delegate (string message, bool validationResult) ValidateSequenceSizeInput(int userInput);
+        private static int ReadIntNumber(string sequenceName, params ValidateSequenceSizeInput[] SequenceInputValidators)
+        {
+            bool isNumberVerified = false;
+            int sequenceSize = -1;
+            string userInput;
+            
+            Console.WriteLine($"Input size of {sequenceName}");
+
+            while (!isNumberVerified)
+            {
+                userInput = Console.ReadLine();
+
+                if (userInput == null)
+                {
+                    Console.WriteLine("Input some value!");
+                    continue;
+                }
+                try
+                {
+                    sequenceSize = Convert.ToInt32(userInput);
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine("Please, input a number!");
+                    continue;
+                }
+
+                (string, bool) validationResult;
+                bool isValidationsPassed = true;
+
+                foreach (var validator in SequenceInputValidators)
+                {
+                    validationResult = validator(sequenceSize);
+                    if (!validationResult.Item2)
+                    {
+                        Console.WriteLine(validationResult.Item1);
+                        isValidationsPassed = false;
+                        break;
+                    }
+                }
+                isNumberVerified = isValidationsPassed ? true : false;
+            }
+            return sequenceSize;
         }
     }
 }
