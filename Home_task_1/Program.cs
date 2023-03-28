2c792c7786274093198b41e83ddd2bfd94e117ef
senivlm <91743906+senivlm@users.noreply.github.com>
2023-03-28 11:29:22 +0300

    Update Program.cs

--- Home_task_1/Program.cs
+++ Home_task_1/Program.cs
@@ -1,6 +1,6 @@
 ﻿using Home_task_1;
 using System.Diagnostics;
-
+//Вітаю. Перше завдання по створенню репозиторію Ви виконали.
 TasksRepresenter.Task1_SpiralMatrix();
 TasksRepresenter.Task2_LongestColorFind();
 
